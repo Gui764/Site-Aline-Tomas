fb1163d968f5e0f6ad250de6fdef33239baf8bde
Guilherme Gabriel Gomes Costa <Guilhermegabriel764@gmail.com>
2025-08-25 15:34:23 -0300

    Restaura o site a partir de um backup .rar antigo

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
+  base: "/",
+  build: {
+    outDir: 'docs' // Esta linha é a mais importante!
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
