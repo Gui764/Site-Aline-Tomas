04ce566701fe1590745ae81c7a27c602647f0fff
Gui764 <Guilhermegabriel764@gmail.com>
2025-08-30 11:45:40 -0300

    Configura o build para a pasta /dist e envia a versão final

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  // --- ADICIONE ESTA LINHA AQUI ---
-  base: "/Site-Aline-Tomas/",
-
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: "/",
+  build: {
+    // Garante que a pasta de saída seja 'dist'
+    outDir: "dist",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
